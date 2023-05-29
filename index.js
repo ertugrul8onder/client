c9b3f6f84aa395201711229443cb7148d05b0859
ertugrulonderDGRoute <ertugrul.onder@dgroute.com>
2023-05-29 13:18:22 +0300

    data

--- index.js
+++ index.js
@@ -333,7 +333,7 @@ const UsersTable = function () {
 
     const getUsers = function () {
         return $.ajax({
-            url: 'http://localhost:3000/data',
+            url: 'https://api.ertugrulonder.com/data',
             type: 'GET',
             dataType: "json",
             contentType: "application/json",
@@ -348,7 +348,7 @@ const UsersTable = function () {
 
     const postUser = function (formData) {
         return $.ajax({
-            url: 'http://localhost:3000/data',
+            url: 'https://api.ertugrulonder.com/data',
             type: 'POST',
             data: JSON.stringify(formData),
             dataType: "text",
